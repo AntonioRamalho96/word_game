862c4a9b6ce7ff5edcfaca78fbca1d2eda9c4aec
AntonioRamalho96 <antonio.ramalho@tecnico.ulisboa.pt>
2024-06-02 17:14:00 +0200

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { KeyboardEventHandler, useEffect, useRef, useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import './App.css'
 import Letter from './Letter.tsx'
 import './Letter.css'
